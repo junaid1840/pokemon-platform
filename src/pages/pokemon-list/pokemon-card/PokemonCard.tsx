683c4f18fb3e2e3f091dae7002bbe0dbb31062ad
junaidnz <junaid.nazir@tintash.com>
2022-06-05 18:44:19 +0500

    Added a redirection link with correct pokemon ID in param.

--- src/pages/pokemon-list/pokemon-card/PokemonCard.tsx
+++ src/pages/pokemon-list/pokemon-card/PokemonCard.tsx
@@ -3,14 +3,23 @@ import { IPokemon } from "../../../redux/interfaces/pokemon/pokemonInterfaces";
 import { Button } from "../../../component/button/Button";
 import { Link } from "react-router-dom";
 import "./pokemonCard.scss";
+import { ROUTES } from "../../../routes/routeConstants";
 
 type IProps = IPokemon;
 
 export const PokemonCard: FC<IProps> = ({ name, url }) => {
+  const getPokemonId = (url: string): string => {
+    const urlSplitArray = url.split("/");
+    return urlSplitArray[urlSplitArray.length - 2];
+  };
+
   return (
     <div className="card-wrapper">
       <h1 className="card-title">{name.toUpperCase()}</h1>
-      <Link to={url} className="redirect-details">
+      <Link
+        to={ROUTES.POKEMON_DETAILS(getPokemonId(url))}
+        className="redirect-details"
+      >
         <Button>Details</Button>
       </Link>
     </div>
