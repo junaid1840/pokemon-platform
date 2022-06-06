a0252d0726ec090c0935516bf22b148a5a5edaff
junaidnz <junaid.nazir@tintash.com>
2022-06-06 14:30:05 +0500

    Added Pokemon details service to fecth pokemon details.

--- src/services/apiServices/pokemonServices.ts
+++ src/services/apiServices/pokemonServices.ts
@@ -1,11 +1,22 @@
 import { apiManager } from "../api-manager/apiManager";
 import { iApiResponse } from "../api-manager/apiManagerInterfaces";
-import { IPokemonListResponse } from "../../redux/interfaces/pokemon/pokemonServiceInterfaces";
+import {
+  IPokemonDetailsResponse,
+  IPokemonListResponse,
+} from "../../redux/interfaces/pokemon/pokemonServiceInterfaces";
 
 export const getPokemonListService = async (
   offsetParams: string
 ): Promise<IPokemonListResponse> => {
   const url = `https://pokeapi.co/api/v2/pokemon/?${offsetParams}`;
+  const { data } = await apiManager.request(url, {}, "GET");
+  return data;
+};
+
+export const getPokemonDetailsService = async (
+  pokemonId: string
+): Promise<IPokemonDetailsResponse> => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
   const { data } = await apiManager.request(url, {}, "GET");
   return data;
 };
